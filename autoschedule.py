a0028f04fb0f7dfa5826c21edf1922a445a9c2dc
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2022-02-21 23:48:04 -0800

    Add files via upload

--- autoschedule.py
+++ autoschedule.py
@@ -11,13 +11,17 @@ def get_RMP(teacher):
     r = requests.get("https://www.ratemyprofessors.com/search/teachers?query=" + teacher + "&sid=U2Nob29sLTE1ODE=")
     s = r.text
     index = s.find("avgRating")
-    rating = float(s[index+11:index+14])
+    if (index == -1): # delete middle name, try again
+        return 0
+    if (s[index+11] == '0'): 
+        return 0
+    rating = float(s[index+11:index+14]) # if this doesnt work return 0
     return rating
 
 def get_course_info(crn): # gets all course info and returns in dictionary
     r = requests.get("https://opencourse.dev/fh/classes/"+str(crn))
     info = r.json() # look at the opencourse API for more info (or just go to the link and look at the formatting)
-    rating = get_RMP(info["instructor"]) # TODO: fix this its not working aaaaaaaaaaaa
+    rating = get_RMP(info['times'][0]['instructor'][0]) # TODO: fix this its not working aaaaaaaaaaaa
     info.update({"Rating": rating})
     return info # return a dictionary with course data
 
@@ -28,4 +32,8 @@ def get_courses(name): #gets all crns and calls course info for each one
     coursedata = [get_course_info(i) for i in data["classes"]]
     return coursedata
 
-get_courses(["MATH", "1A"])
+print(get_courses(["MATH", "2A"]))
+
+
+
+# maybe check for middle name????????????
