232889749c09edcfae060a1e55f8d9d3ad3a8251
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2022-02-22 00:18:55 -0800

    got it working
    got it working, as well as changing the call to RMP from inside get_course_info to inside get_courses so I can later add functionality for De Anza as well as foothill.
    -j

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # autoschedule
  
+use python -m pip install requests to install requests, the only dependancy at this time
+
+this is a WORK IN PROGRESS. I will get this to create a schedule LATER. STOP ASKING. 
+
+
+
+
+
+
+
---
+++ autoschedule.py
@@ -0,0 +1,40 @@
+import requests;
+
+def format_input(input): # input should be formatted as "MATH 1A, PSYCH 2B..."
+    classes = input.split(", ") # seperates multiple course names
+    classes = [i.split(" ") for i in classes] # seperates MATH from 1A
+    return classes
+
+def get_RMP(teacher):
+    #will break if multiple teachers have the same name but fuck you anyways
+    teacher = teacher.replace(" ", "%20")
+    r = requests.get("https://www.ratemyprofessors.com/search/teachers?query=" + teacher + "&sid=U2Nob29sLTE1ODE=")
+    s = r.text
+    index = s.find("avgRating")
+    if (index == -1): # delete middle name, try again
+        return 0
+    if (s[index+11] == '0'): 
+        return 0
+    rating = float(s[index+11:index+14]) 
+    return rating
+
+def get_course_info(crn): # gets all course info and returns in dictionary
+    r = requests.get("https://opencourse.dev/fh/classes/"+str(crn))
+    info = r.json() # look at the opencourse API for more info (or just go to the link and look at the formatting)
+
+    return info # return a dictionary with course data
+
+def get_courses(name): #gets all crns and calls course info for each one
+    # name should be a list with [0] being the department and [1] being the course (e.g. ["MATH", "1A"])
+    r = requests.get("https://opencourse.dev/fh/depts/" + name[0].upper() + "/courses/" + name[1].upper())
+    data = r.json()
+    coursedata = [get_course_info(i) for i in data["classes"]]
+    rating = get_RMP(coursedata['times'][0]['instructor'][0])
+    coursedata.update({"Rating": rating})
+    return coursedata
+
+print(get_courses(["MATH", "2A"]))
+
+
+
+# maybe check for middle name????????????
