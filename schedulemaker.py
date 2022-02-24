46367c9cb2be3836de1177d8fb078ba75df2c0e5
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2022-02-23 17:39:37 -0800

    commit

--- schedulemaker.py
+++ schedulemaker.py
@@ -64,10 +64,10 @@ def get_courses_formatted(s):
 
 # print("\n".join([i.toString() for i in get_courses_formatted("CS 2A,CS 2B")])) # doesnt work anymore because of how this is indiced
 # print(get_courses_formatted("CS 2A,CS 2B"))
-courselist = [
-    {'CRN': 40165, 'raw_course': 'C S F002A01Z', 'dept': 'CS', 'course': '2A', 'section': '01Z', 'title': 'Object-Oriented Programming Methodologies in C++', 'units': 4.5, 'start': '04/04/2022', 'end': '06/24/2022', 'times': [{'type': 'Lecture', 'days': 'TTh', 'start_time': '08:00 AM', 'end_time': '09:50 AM', 'instructor': ['Anand Venkataraman'], 'location': 'Foothill, Main Campus ONLINE'}, {'type': 'Lab', 'days': 'TBA', 'start_time': 'TBA', 'end_time': 'TBA', 'instructor': ['Anand Venkataraman'], 'location': 'Foothill, Main Campus ONLINE'}], 'status': 'open', 'seats': 40, 'wait_seats': 10, 'wait_cap': 10, 'rating': 3.6},  
-    {'CRN': 40166, 'raw_course': 'C S F002A03W', 'dept': 'CS', 'course': '2A', 'section': '03W', 'title': 'Object-Oriented Programming Methodologies in C++', 'units': 4.5, 'start': '04/04/2022', 'end': '06/24/2022', 'times': [{'type': 'Lecture', 'days': 'TBA', 'start_time': 'TBA', 'end_time': 'TBA', 'instructor': ['David Lee Harden'], 'location': 'Foothill, Main Campus ONLINE'}, {'type': 'Lab', 'days': 'TBA', 'start_time': 'TBA', 'end_time': 'TBA', 'instructor': ['David Lee Harden'], 'location': 'Foothill, Main Campus ONLINE'}], 'status': 'open', 'seats': 38, 'wait_seats': 10, 'wait_cap': 10, 'rating': 2.7},
-]
+#courselist = [
+#    {'CRN': 40165, 'raw_course': 'C S F002A01Z', 'dept': 'CS', 'course': '2A', 'section': '01Z', 'title': 'Object-Oriented Programming Methodologies in C++', 'units': 4.5, 'start': '04/04/2022', 'end': '06/24/2022', 'times': [{'type': 'Lecture', 'days': 'TTh', 'start_time': '08:00 AM', 'end_time': '09:50 AM', 'instructor': ['Anand Venkataraman'], 'location': 'Foothill, Main Campus ONLINE'}, {'type': 'Lab', 'days': 'TBA', 'start_time': 'TBA', 'end_time': 'TBA', 'instructor': ['Anand Venkataraman'], 'location': 'Foothill, Main Campus ONLINE'}], 'status': 'open', 'seats': 40, 'wait_seats': 10, 'wait_cap': 10, 'rating': 3.6},  
+#    {'CRN': 40166, 'raw_course': 'C S F002A03W', 'dept': 'CS', 'course': '2A', 'section': '03W', 'title': 'Object-Oriented Programming Methodologies in C++', 'units': 4.5, 'start': '04/04/2022', 'end': '06/24/2022', 'times': [{'type': 'Lecture', 'days': 'TBA', 'start_time': 'TBA', 'end_time': 'TBA', 'instructor': ['David Lee Harden'], 'location': 'Foothill, Main Campus ONLINE'}, {'type': 'Lab', 'days': 'TBA', 'start_time': 'TBA', 'end_time': 'TBA', 'instructor': ['David Lee Harden'], 'location': 'Foothill, Main Campus ONLINE'}], 'status': 'open', 'seats': 38, 'wait_seats': 10, 'wait_cap': 10, 'rating': 2.7},
+#]
 
-sched = Schedule([Class(i) for i in courselist])
-print(sched.toString())
+#sched = Schedule([Class(i) for i in courselist])
+#print(sched.toString())
